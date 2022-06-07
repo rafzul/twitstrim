6bde43d2e14cf37166394624e8dbefcc585c29f9
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-06-07 23:53:50 +0700

    setup

---
+++ keys.py
@@ -0,0 +1,4 @@
+consumer_key="mm8xZUnyKX8jIhiDoLPHIOGMg"
+consumer_secret="FLrB5Cy9DQRJnzCTuIqQmih9kH9HxFu7MCPTpCpWxpENh55s6t"
+access_token="1288097192385757184-AkxCzJP5AkYFXJH3el8r9pkLFJ6YA0"
+access_token_secret="zIyep1QXQo7LVIpnGWbyda0doVjMEgXixW1EvnkeeD38f"
